9dab2731ede832810ab90d720aa857306dfc3ad5
bingomickey <jiaqiyuan7@gmail.com>
2015-11-15 15:17:05 -0800

    Leetcode: 034 Search for a Range
    Given a sorted array of integers, find the starting and ending position
    of a given target value.
    Your algorithm's runtime complexity must be in the order of O(log n).
    If the target is not found in the array, return [-1, -1].
    For example,
    Given [5, 7, 7, 8, 8, 10] and target value 8, return [3, 4].

---
+++ 034_Search_for_a_Range.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+            
+        return self.compare(root.left, root.right)
+        
+    def compare(self, left, right):
+        if left is None and right is None:
+            return True
+            
+        if left is None or right is None:
+            return False
+            
+        return left.val == right.val and self.compare(left.left, right.right) and self.compare(left.right, right.left)
+		
